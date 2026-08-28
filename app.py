25476f710adfd036be3f515137a74246d38f3eb6
Asuvathaman <asuvathaman2005@gmail.com>
2026-08-29 00:34:20 +0530

    Update homepage from main branch

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def home():
     <body>
         <div class="container">
             <h1>Dockerized Web Application</h1>
-            <p>CodSoft DevOps Internship - Git & GitHub Workflow</p>
+            <p>CodSoft DevOps Internship - Main Branch Update</p>
             <p>Application successfully managed using Git and GitHub!</p>
         </div>
     </body>
